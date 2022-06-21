382a56eb307bb5c97a4b80713ea5814125f3c188
dawe <danielweber1992@outlook.de>
2022-06-21 20:27:24 +0200

    add animation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 const bgImage = document.querySelector('.bg__img');
 const containerImages = document.querySelectorAll('.container__image');
 
-bgImage.addEventListener('mouseenter', () => {
-    bgImage.style.backgroundImage = 'none';
-});
-
 containerImages.forEach(ci => ci.addEventListener('mouseenter', () => {
     const ciStyle = getComputedStyle(ci);
+    bgImage.style.animation = 'none';
     bgImage.style.backgroundImage = ciStyle?.backgroundImage;
-}))
+    bgImage.style.animation = 'bg-animation 3s ease-out forwards';
+}));
